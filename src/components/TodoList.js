24ea666c372d0b845f589d267c911afa723289d5
evzheniy <evzheniy@yandex.ru>
2016-06-03 19:44:35 +0300

    huita kakayato

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,19 +1,8 @@
 import React from 'react'
-import {TodoItem} from 'components'
 
 class TodoList extends React.Component {
   render () {
-    return <div className="TodoList">
-      {
-        this.props.tasks.map((task)=>(<TodoItem
-          text={task.text}
-          key={task.id}
-          id={task.id}
-          checked={task.checked}
-          />
-        ))
-      }
-    </div>
+    return (<div className="TodoList">бла-бла бла </div>)
   }
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
-import VisibleTodoList from './VisibleTodoList'
+import TodoList from 'components'
+
+console.log(TodoList);
+
 export class App extends Component {
   render() {
     return <div>
       <h1>TODO (based on redux)</h1>
-      <VisibleTodoList />
+      <TodoList />
     </div>
   }
 }
